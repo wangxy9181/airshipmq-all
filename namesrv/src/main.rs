cd0292b74ca0ac675503a56aa7e52fe3b870153a
wangxy9181 <1275289939@qq.com>
2023-06-29 17:33:12 +0800

    namesrv broker实现

--- namesrv/src/main.rs
+++ namesrv/src/main.rs
@@ -1,5 +1,6 @@
 mod metadata;
 mod process;
+mod config;
 
 fn main() {
     println!("Hello, world!");
--- namesrv/src/process/processor.rs
+++ namesrv/src/process/processor.rs
@@ -3,10 +3,32 @@ use remoting::{RemotingCommand, RemotingError, RemotingProcess};
 
 pub struct NamesrvProcessor;
 
+impl NamesrvProcessor {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
 #[async_trait]
 impl RemotingProcess for NamesrvProcessor {
 
     async fn process(&self, cmd: RemotingCommand) -> Result<RemotingCommand, RemotingError> {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use chrono::Local;
+
+    #[tokio::test]
+    async fn process_should_work() -> Result<()> {
+        // 创建
+        // 创建 Namesrv
+        let namesrv = NamesrvProcessor::new();
+        //
+        Ok(())
     }
 }
